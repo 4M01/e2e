32546834ff1a50d5b309e6f3fa5a3a68a46f57fd
Amol Chavan <2amolchavan@gmail.com>
2022-06-22 12:00:36 +0530

    chore; changes screenshot stratgey

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,7 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-    screenshot: "on",
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
